ae6a72b8f92d1ce70dc2f3d62ae9e94ba1a25f69
Titi L <theo.lemarre@gmail.com>
2023-10-02 18:31:18 +0200

    Phase 2: premier test(Hello World !)

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AcceuilController
+ *
+ * @author Titi L
+ */
+class AcceuilController {
+    
+    /**
+     * @Route("/", name="acceuil")
+     * @return Response
+     */
+    public function index() : Response{
+        return new Response('Hello World !');
+    }
+}
